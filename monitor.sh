28ecc2089c3e5f872efa02fa43a3151459a6868b
Mudassir <mudassirpak47@gmail.com>
2026-03-28 12:27:01 -0400

    add check_disk function

---
+++ monitor.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+
+source "$(dirname "$0")/config.env"
+
+mkdir -p "$(dirname "$REPORT_LOG")"
+
+trap 'echo "ERROR : SCRIPT FAILED....." >> "$ALERT_LOG"' ERR
+
+
+timestamp() {
+	date "+%Y-%m-%d %H:%M:%S"
+}
+
+log() {
+	echo "$timestamp $1" >> "$REPORT_LOG"
+}
+
+alert() {
+	echo "$timestamp ALERT: $1" >> "$ALERT_LOG"
+	log "ALERT TRIGGERED: $1"
+}
+
+check_disk() {
+	local usage 
+	usage=$( df / | awk ' NR==2 {gsub("%",""); print $5 } ' )
+
+	log "Disk usage: ${usage}%"
+	
+	if [ $usage -ge $DISK_THRESHOLD ];
+then
+	alert "Disk usage is ${usage}% - exceeds disk threshold of  ${DISK_THRESHOLD}% "
+
+fi
+}
+
